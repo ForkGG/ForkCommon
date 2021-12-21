73cd465140c0349d442861bff34751e3c9397e3e
ChristianKern97 <ikerneii97@gmail.com>
2021-12-21 13:22:16 +0100

    Create EntityStatusChangedNotification.cs

---
+++ Model/Notifications/EntityNotifications/EntityStatusChangedNotification.cs
@@ -0,0 +1,11 @@
+﻿using ProjectAveryCommon.Model.Entity.Enums;
+using ProjectAveryCommon.Model.Privileges;
+using ProjectAveryCommon.Model.Privileges.Entity.ReadEntity;
+
+namespace ProjectAveryCommon.Model.Notifications.EntityNotifications;
+
+[Privileges(typeof(IReadEntityPrivilege))]
+public class EntityStatusChangedNotification : AbstractEntityNotification
+{
+    public EntityStatus NewEntityStatus { get; set; }
+}
