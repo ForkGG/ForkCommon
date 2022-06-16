c3600abfd044acb1897c96b843f8f6cb343a2b2f
ChristianKern97 <ikerneii97@gmail.com>
2022-06-16 15:38:45 +0200

    Auto initialize fields

--- Model/Payloads/Entity/CreateServerPayload.cs
+++ Model/Payloads/Entity/CreateServerPayload.cs
@@ -6,8 +6,8 @@ namespace ForkCommon.Model.Payloads.Entity;
 public class CreateServerPayload
 {
     public string ServerName { get; set; } = "Server";
-    public ServerVersion ServerVersion { get; set; }
-    public VanillaSettings VanillaSettings { get; set; }
-    public JavaSettings JavaSettings { get; set; }
+    public ServerVersion ServerVersion { get; set; } = new ServerVersion();
+    public VanillaSettings VanillaSettings { get; set; } = new VanillaSettings("world");
+    public JavaSettings JavaSettings { get; set; } = new JavaSettings();
     public string WorldPath { get; set; } = null;
 }
